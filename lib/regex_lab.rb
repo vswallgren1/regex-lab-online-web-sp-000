4bdbe915916ffb4ee640b506b18cb281f4c8b926
Vince W <vincewallgren@me.com>
2020-11-18 03:41:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+    return true
 end
 
 def valid_phone_number?(phone)
